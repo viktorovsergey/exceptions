749dbeca37229e8830af4d6dc7ab6c98aaf78882
ya_zabiyaka <ya_zabiyaka@bk.ru>
2023-04-24 21:38:48 +0300

    работу закончил

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            accountInput("Login1ваааааа", "password", "password");
+        } catch (WrongPasswordException e) {
+            throw new RuntimeException(e);
+        } catch (WrongLoginException e) {
+            throw new RuntimeException(e);
+        }finally {
+            System.out.println("блок finnaly выполнен");
+        }
+
+    }
+    private static void  accountInput (String login, String password, String confirmPassword){
+        if (!login.matches("[a-zA-Z0-9_]+"))
+            throw new WrongLoginException();
+        if (login.length()>=20){
+            throw new WrongLoginException();
+        }
+        if (password.length()>20||!password.equals(confirmPassword)){
+            throw new WrongPasswordException();
+        }
     }
 }
